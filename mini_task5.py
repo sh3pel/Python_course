708e9bb1e09633679782b2ae36712e408c342b3f
Shepel Vladislav <150267649+sh3pel@users.noreply.github.com>
2024-10-22 23:45:16 +0700

    Update task 5 after feedback

--- mini_task5.py
+++ mini_task5.py
@@ -1,21 +1,22 @@
 import unittest
+
 def specialize(func, *args, **kwargs):
-    def sub_spec(*more_args):
+    def sub_spec(*more_args, **more_kwargs): 
         all_args = args + more_args
-        return func(*all_args, **kwargs)
+        all_kwargs = {**kwargs, **more_kwargs} 
+        return func(*all_args, **all_kwargs)
     return sub_spec
 
-def sum(x, y):
-    return x + y
+def custom_sum(*args, **kwargs):
+    return sum(args) + sum(kwargs.values())
 
 class TestReversedDict(unittest.TestCase):
     def test_valid_input(self):
-        plus_one = specialize(sum, y=1)
-        self.assertEqual(plus_one(10), 11 , "Failed test")
+        plus_three = specialize(custom_sum, y=1)
+        self.assertEqual(plus_three(10, z=5, a = 15), 31, "Failed test")
         
-        just_two = specialize(sum, 1, 1)
+        just_two = specialize(custom_sum, 1, 1)
         self.assertEqual(just_two(), 2, "Failed test")
 
 if __name__ == '__main__':
     unittest.main()
-
