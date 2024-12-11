9512548bd5e59f7e11bc765a31be0c90eb64627a
Shepel Vladislav <150267649+sh3pel@users.noreply.github.com>
2024-12-11 07:26:51 +0300

    Update mini_task11.py

--- mini_task11.py
+++ mini_task11.py
@@ -1,19 +1,22 @@
 def cycle(iterable):
-    while True: 
-        for item in iterable:
-            yield item
+    items = list(iterable)
+    while True:
+        yield from items
 
 def take(generator, n):
     return [next(generator) for _ in range(n)]
 
 def chain(*iterables):
     for iterable in iterables:
-        for item in iterable:
-            yield item  
+        yield from iterable
 
 if __name__ == "__main__":
     result = take(cycle([1, 2, 3]), 10)
-    print(result)
+    print(result)  
+
     my_list = [42, 13, 7]
     result = list(chain([1, 2, 3], ['a', 'b'], my_list))
+    print(result)
+
+    result = take(cycle(chain([1, 2, 3], ["A", "c"], ("Z", "f"))), 10)
     print(result)
